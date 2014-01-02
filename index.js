cf2018c9877806c585ccfaed7e2bb3e3ef2df71b
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-01-02 11:39:26 +0000

    Initial import

---
+++ index.js
@@ -0,0 +1,15 @@
+var execute = require('plumber').execute;
+
+var flatten = require('flatten');
+
+
+module.exports = function(/* pipelinesOrOperations... */) {
+    // Turn all operations into pipelines (i.e. arrays)
+    var pipelinesOrOperations = [].slice.call(arguments);
+    var pipelines = pipelinesOrOperations.map(function(pipeOrOp) {
+        return flatten([pipeOrOp]);
+    });
+
+    // FIXME: hacky!
+    return pipelines;
+};
