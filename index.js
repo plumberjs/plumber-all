c940783052ceed9c0aad38a1ef0c206b48e4d919
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-01-12 19:02:44 +0000

    Move repository to plumberjs org

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-var execute = require('plumber').execute;
-
 var flatten = require('flatten');
 
-
 module.exports = function(/* pipelinesOrOperations... */) {
-    // Turn all operations into pipelines (i.e. arrays)
+    // Turn all operations into flat pipelines (i.e. arrays)
     var pipelinesOrOperations = [].slice.call(arguments);
     var pipelines = pipelinesOrOperations.map(function(pipeOrOp) {
-        return flatten([pipeOrOp]);
+        return flatten([pipeOrOp], 1);
     });
 
     // FIXME: hacky!
