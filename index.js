b746d6efaeeeec68508a7e4166d43e084576305c
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-01-12 19:14:31 +0000

    Undo change, not ready yet

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var flatten = require('flatten');
 
 module.exports = function(/* pipelinesOrOperations... */) {
-    // Turn all operations into flat pipelines (i.e. arrays)
+    // Turn all operations into pipelines (i.e. arrays)
     var pipelinesOrOperations = [].slice.call(arguments);
     var pipelines = pipelinesOrOperations.map(function(pipeOrOp) {
-        return flatten([pipeOrOp], 1);
+        return flatten([pipeOrOp]);
     });
 
     // FIXME: hacky!
