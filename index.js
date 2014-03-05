b054f283100d9abb2b129bf7683ab8c7101933c0
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-03-05 00:49:07 +0000

    Update to plumber 0.3

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
+var assemble = require('plumber').assemble;
+
+var highland = require('highland');
 var flatten = require('flatten');
 
-function asArray(valOrArray) {
-    if (Array.isArray(valOrArray)) {
-        return valOrArray;
-    } else {
-        return [valOrArray];
-    }
+// TODO: submit to Highland?
+function zipAll(streams) {
+    return streams.reduce(highland.zip).map(function(pairs) {
+        return flatten([pairs]);
+    });
 }
 
-module.exports = function(/* pipelinesOrOperations... */) {
-    // Turn all operations into pipelines (i.e. arrays)
-    var pipelinesOrOperations = [].slice.call(arguments);
-    return pipelinesOrOperations.map(asArray);
-};
+function all(/* operations... */) {
+    var operations = [].slice.call(arguments);
+    return function(executions) {
+        var pipelines = operations.map(function(op) {
+            // ensure streams are forked for each listener
+            return assemble(op, executions.fork().invoke('fork'));
+        });
+        return zipAll(pipelines).map(highland.merge);
+        // TODO: or should we use parallel?
+        // return zipAll(operators).map(function(allExecutions) {
+        //   return highland(allExecutions).parallel(allExecutions.length);
+        // });
+    };
+}
+
+module.exports = all;
